b407f8049f66c10391fc224302015a0b07421a5a
Keanu Barnard <keanu.barnardd@gmail.com>
2023-02-06 15:04:34 +1100

    Fav and general re style

--- contactopedia/src/Components/ContactPages/FavouriteContact.jsx
+++ contactopedia/src/Components/ContactPages/FavouriteContact.jsx
@@ -3,10 +3,12 @@ import Contact from "./Contact";
 
 export default function FavouriteContact(props) {
   return (
-    <div>
-      {props.contacts.map((obj, index) => (
-        <Contact contact={obj} key={index} />
-      ))}
+    <div className="col-12 py-2" style={{ borderRadius: "10px", backgroundColor: "#323637" }}>
+      <div className="p-2">
+        {props.contacts.map((obj, index) => (
+          <Contact contact={obj} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
--- contactopedia/src/Components/ContactPages/GeneralContact.jsx
+++ contactopedia/src/Components/ContactPages/GeneralContact.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import Contact from "./Contact";
 export default function GeneralContact(props) {
   return (
-    <div>
-      {props.contacts.map((obj, index) => (
-        <Contact contact={obj} key={index} />
-      ))}
+    <div className="col-12 py-2" style={{ borderRadius: "10px", backgroundColor: "#323637" }}>
+      <div className="p-2">
+        {props.contacts.map((obj, index) => (
+          <Contact contact={obj} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
