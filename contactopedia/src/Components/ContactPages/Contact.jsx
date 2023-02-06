22c870c97d674a735c9403a84eb2d6751fc105a5
Keanu Barnard <keanu.barnardd@gmail.com>
2023-02-06 15:00:45 +1100

    Contact buttons added

--- contactopedia/src/Components/ContactPages/Contact.jsx
+++ contactopedia/src/Components/ContactPages/Contact.jsx
@@ -23,9 +23,18 @@ export default function Contact(props) {
           className={`btn btn-sm m-1 ${
             props.contact.isFavorite ? "btn-warning" : "btn-outline-warning"
           }`}
-        ><i className="bi bi-star" style={{fontSize:"1.3rem"}}></i></button>
+        >
+          <i className="bi bi-star" style={{ fontSize: "1.3rem" }}></i>
+        </button>
       </div>
-      <button className="btn btn-secondary form-control">{props.contact.name}</button>
+      <div className="col-2 col-md-3 pt-md-3">
+        <button className="btn btn-primary btn-sm m-1">
+          <i className="bi bi-pencil-square" style={{ fontSize: "1.3rem" }}></i>
+        </button>
+        <button className="btn btn-danger btn-sm m-1">
+          <i className="bi bi-trash-fill" style={{ fontSize: "1.3rem" }}></i>
+        </button>
+      </div>
     </div>
   );
 }
