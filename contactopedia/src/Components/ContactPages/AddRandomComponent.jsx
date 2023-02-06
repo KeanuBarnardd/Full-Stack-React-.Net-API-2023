ce7b6d54df83624ad3f14c2e2df41510d1f3f8a9
Keanu Barnard <keanu.barnardd@gmail.com>
2023-02-06 14:05:44 +1100

    Add all components

---
+++ contactopedia/src/Components/ContactPages/AddRandomComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddRandomComponent() {
+  return (
+    <div>AddRandomComponent</div>
+  )
+}
---
+++ contactopedia/src/Components/Layout/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="py-2 pl-2" style={{ borderBottom: "1px solid #77" }}>
+      <span className="h2 pt-4 m-2">Contactopedia</span>
+    </div>
+  );
+}
