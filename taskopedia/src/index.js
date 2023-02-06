7f60c9e42a98d38614d48eaa8ad49dc5f2954dcf
Keanu Barnard <keanu.barnardd@gmail.com>
2023-02-06 12:31:39 +1100

    Basic Component Setup

--- taskopedia/src/index.js
+++ taskopedia/src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function MainBody() {
+  return (
+    <div>
+      <p>IN this course, we will lean how to be cool</p>
+      <ul>
+        <li>Call yo mama house</li>
+        <li>Go to wallmart</li>
+      </ul>
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>React Course</h1>;
+}
+
+function Footer() {
+  return <p>Happy Coding!</p>;
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Header />
+    <MainBody />
+    <Footer />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
