f15a875425a2700021fa00bbab2853558c2e768a
Keanu Barnard <keanu.barnardd@gmail.com>
2023-02-07 14:27:30 +1100

    Add Localstorage saving

--- lifecylepedia/src/CycloPediaClassPage.jsx
+++ lifecylepedia/src/CycloPediaClassPage.jsx
@@ -10,27 +10,50 @@ export default class CycloPediaClassPage extends Component {
       studentList: [],
       studentCount: 0,
       hideInstructor: false,
+      inputName: "",
+      inputFeedback: "",
     };
   }
 
-  componentDidMount = async () => {
-    console.log("Component Did Mount");
-    const response = await getRandomUser();
-    console.log(response);
-
+  handleAddStudent = () => {
     this.setState((prevState) => {
       return {
-        instructor: {
-          name: response.data.first_name + " " + response.data.last_name,
-          email: response.data.email,
-          phone: response.data.phone_number,
-        },
+        studentCount: prevState.studentCount + 1,
       };
     });
   };
 
+  handleRemoveAllStudents = () => {
+    this.setState((prevState) => {
+      return {
+        studentCount: 0,
+      };
+    });
+  };
+
+  componentDidMount = async () => {
+    console.log("Component Did Mount");
+    if (JSON.parse(localStorage.getItem("cylcopediaState"))) {
+      this.setState(JSON.parse(localStorage.getItem("cylcopediaState")));
+    } else {
+      const response = await getRandomUser();
+      console.log(response);
+
+      this.setState((prevState) => {
+        return {
+          instructor: {
+            name: response.data.first_name + " " + response.data.last_name,
+            email: response.data.email,
+            phone: response.data.phone_number,
+          },
+        };
+      });
+    }
+  };
+
   componentDidUpdate() {
     console.log("Component Did Update");
+    localStorage.setItem("cylcopediaState", JSON.stringify(this.state));
   }
 
   componentWillUnmount() {
@@ -53,6 +76,39 @@ export default class CycloPediaClassPage extends Component {
             Phone:{this.state.instructor.phone}
           </div>
         )}
+        <div className="p-3">
+          <span className="h4 text-success">Feedback</span>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => this.setState({ inputName: e.target.value })}
+            value={this.state.inputName}
+            placeholder="Name..."
+          ></input>
+          Value: {this.state.inputName}
+          <br />
+          <textarea
+            name=""
+            onChange={(e) => this.setState({ inputFeedback: e.target.value })}
+            value={this.state.inputFeedback}
+            placeholder="feedback"
+            id=""
+            cols="30"
+            rows="3"
+          ></textarea>
+          Value: {this.state.inputFeedback}
+        </div>
+        <div className="p-3">
+          <span className="h4 text-success">Students</span> <br />
+          <div>Student Count: {this.state.studentCount}</div>
+          <button onClick={this.handleAddStudent} className="btn btn-success btn-sm">
+            Add Student
+          </button>
+          &nbsp;
+          <button onClick={this.handleRemoveAllStudents} className="btn btn-danger btn-sm">
+            Remove All Student
+          </button>
+        </div>
       </div>
     );
   }
---
+++ lifecylepedia/src/instructor.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class instructor extends Component {
+  render() {
+    return <div>instructor</div>;
+  }
+}
